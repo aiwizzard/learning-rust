0ec20637a9fc05149156a0359333fc276083eb95
aiwizzard <ajmalekvsroad@gmail.com>
2021-11-10 21:49:22 +0530

    arrays

---
+++ src/arrays.rs
@@ -0,0 +1,28 @@
+// Fixed list where elements are the same data types
+
+use std::mem;
+
+pub fn run() {
+    let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Re-assign a value
+    numbers[2] = 25;
+
+    println!("{:?}", numbers);
+
+    // Get single value
+    println!("First value: {}", numbers[0]);
+
+    // Get array length
+    println!("Array Length: {}", numbers.len());
+
+    // Arrays are stack allocated
+    println!(
+        "Array occupies {} bytes in memory",
+        mem::size_of_val(&numbers)
+    );
+
+    // Get slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice);
+}
